1abc199aa4787f4be2fbe255f51030fb0819ddcb
gids-dadi <gideons564@gmail.com>
2022-10-18 14:05:07 +0100

    Add and configure Ability for cancan authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,13 @@
 class ApplicationController < ActionController::Base
 
-   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
-  skip_before_action :verify_authenticity_token
+before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   protected
 
-  def after_sign_out_path_for(_resource_or_scope)
-    new_user_session_path
-  end
-
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: %i[email password])
+    devise_parameter_sanitizer.permit(:sign_up, keys:
+       %i[name email password password_confirmation])
+  
   end
 end
